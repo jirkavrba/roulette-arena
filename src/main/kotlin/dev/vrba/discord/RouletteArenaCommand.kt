c810c2d609379c4d2c3a3d6a0d86c2db505ec512
Jiří Vrba <jiri@vrba.dev>
2024-06-14 18:05:19 +0200

    Add a required token parameter for running the bot

--- src/main/kotlin/dev/vrba/discord/RouletteArenaCommand.kt
+++ src/main/kotlin/dev/vrba/discord/RouletteArenaCommand.kt
@@ -1,29 +1,28 @@
 package dev.vrba.discord
 
 import io.micronaut.configuration.picocli.PicocliRunner
-import io.micronaut.context.ApplicationContext
-
-import picocli.CommandLine
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
-import picocli.CommandLine.Parameters
 
-@Command(name = "roulette-arena", description = ["..."],
-        mixinStandardHelpOptions = true)
+@Command(
+    name = "roulette-arena",
+    description = ["..."],
+    mixinStandardHelpOptions = true,
+)
 class RouletteArenaCommand : Runnable {
-
-    @Option(names = ["-v", "--verbose"], description = ["..."])
-    private var verbose : Boolean = false
+    @Option(
+        names = ["-t", "--token"],
+        description = ["Discord token that the bot should run with"],
+        required = true,
+    )
+    private lateinit var token: String
 
     override fun run() {
-        // business logic here
-        if (verbose) {
-            println("Hi!")
-        }
     }
 
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
+        @JvmStatic
+        fun main(args: Array<String>) {
             PicocliRunner.run(RouletteArenaCommand::class.java, *args)
         }
     }
